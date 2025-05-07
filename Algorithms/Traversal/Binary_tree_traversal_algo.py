7f44ece07d927b0e186d9fd75bae8f006956b57b
R-Jeevan-R <jeevanrjnvc@gmail.com>
2025-05-07 12:50:33 +0530

    Removed Comments

--- Algorithms/Traversal/Binary_tree_traversal_algo.py
+++ Algorithms/Traversal/Binary_tree_traversal_algo.py
@@ -1,43 +1,30 @@
-'''Tree Traversal algorithms :
-Inorder -- (Left,Root,Right) -- Traverses left child first followed by root and then right child at each node.
-Preorder -- (Root,Left,Right) -- Traverses root first followed by left child and then right child at each node.
-Postorder -- (Left,Right,Root) --- Traverses left child first followed by right child and root at last at each node.'''
+'''Tree Traversal algorithms:
+Inorder -- (Left, Root, Right)
+Preorder -- (Root, Left, Right)
+Postorder -- (Left, Right, Root)
+'''
 
-##class Node():
-##    def __init__(self,data):
-##        self.left=None
-##        self.data=data
-##        self.right=None
-##
-##class Binary_tree():
-##    def __init__(self,root_key):
-##        self.root=Node(root_key)
-
-class Traversals():
+class Traversals:
     def __init__(self):
         pass
 
-    #Time Complexity -- O(n)
-    def inorder(self,root):
+    # Time Complexity: O(n)
+    def inorder(self, root):
         if root:
             self.inorder(root.left)
-            print(root.data,end=' ')
+            print(root.data, end=' ')
             self.inorder(root.right)
-            
-    #Time Complexity -- O(n)
-    def preorder(self,root):
+
+    # Time Complexity: O(n)
+    def preorder(self, root):
         if root:
-            print(root.data,end=' ')
+            print(root.data, end=' ')
             self.preorder(root.left)
             self.preorder(root.right)
-            
-    #Time Complexity -- O(n)
-    def postorder(self,root):
+
+    # Time Complexity: O(n)
+    def postorder(self, root):
         if root:
             self.postorder(root.left)
             self.postorder(root.right)
-            print(root.data,end=' ')
-
-     
-            
-            
+            print(root.data, end=' ')
